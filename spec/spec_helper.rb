843b208f0e5f04b005c6b82dfc92a3a147e7bb45
Travis Petticrew <travis@petticrew.net>
2015-02-24 14:00:30 -0600

    Turned off warnings
    I was getting several uninitialized instance variable warnings in
    dependent libraries.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,7 +20,7 @@ RSpec.configure do |config|
   config.filter_run :focus
   config.run_all_when_everything_filtered = true
   config.disable_monkey_patching!
-  config.warnings = true
+  config.warnings = false
   config.order = :random
   Kernel.srand config.seed
 
