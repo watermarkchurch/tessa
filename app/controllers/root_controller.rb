df876ff8816fb64fcbd79671a133d539cefe29b4
Travis Petticrew <travis@petticrew.net>
2015-08-26 14:53:06 -0500

    Adds a little more info to the "It works!" page

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,5 +1,11 @@
 class RootController < Sinatra::Base
   get "/" do
-    "It works!"
+    %{
+      <h1>It works!</h1>
+      <h2>Strategies:</h2>
+      <pre>#{STRATEGIES.strategies.keys.to_json}</pre>
+      <h2>Assets:</h2>
+      <strong>Total:</strong> #{DB[:assets].count}
+    }
   end
 end
