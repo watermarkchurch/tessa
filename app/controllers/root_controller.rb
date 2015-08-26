799a53c3851f489462d5edab585f09e44188c993
Travis Petticrew <travis@petticrew.net>
2015-08-26 16:45:32 -0500

    Adds a little more data to the status page

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,11 +1,16 @@
 class RootController < Sinatra::Base
   get "/" do
+    asset_status_counts = Asset::STATUSES.collect do |status, status_id|
+      count = DB[:assets].where(status_id: status_id).count
+      "<div><strong>#{status}:</strong> #{count}</div>"
+    end
     %{
       <h1>It works!</h1>
       <h2>Strategies:</h2>
       <pre>#{STRATEGIES.strategies.keys.to_json}</pre>
       <h2>Assets:</h2>
-      <strong>Total:</strong> #{DB[:assets].count}
+    #{asset_status_counts.join}
+      <div><strong>Total:</strong> #{DB[:assets].count}</div>
     }
   end
 end
