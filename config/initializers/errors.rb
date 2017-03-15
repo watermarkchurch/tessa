c96e5bb7c5c94b8377263cc8516d35bd6c3430c2
Travis Petticrew <travis@petticrew.net>
2017-03-15 17:03:24 -0500

    Configure Bugsnag release stage from environment

--- config/initializers/errors.rb
+++ config/initializers/errors.rb
@@ -1,5 +1,6 @@
 if defined?(Bugsnag) && ENV['BUGSNAG_API_KEY']
   Bugsnag.configure do |config|
     config.api_key = ENV['BUGSNAG_API_KEY']
+    config.release_stage = ENV['BUGSNAG_RELEASE_STAGE'] || ENV['RACK_ENV']
   end
 end
